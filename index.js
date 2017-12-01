b88e56e8524ee77862981399fd4320d7f97dabbd
Sean O'Grady <sean@miralize.com>
2017-12-01 17:18:32 +0000

    test

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ import {Client} from 'discord.js'
 const client = new Client()
 const token = process.env.DISCORD_BOT_TOKEN
 
-client.on('ready', () => {
-    console.log('Ready!');
-})
-
 client.on('message', message => {
     if (message.content === 'ping') {
         message.reply('pong');

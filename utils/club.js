46b78a0c36aea953c6f438fce9d618f0ed5df299
Sean O'Grady <sean@miralize.com>
2017-12-06 18:20:13 +0000

    Update club.js

--- utils/club.js
+++ utils/club.js
@@ -1,5 +1,5 @@
 exports.parseRecord = (s) => {
-  const parseNum = num => (num === '0' ? ':regional_indicator_w:' : num === '1' ? ':regional_indicator_d:' : ':regional_indicator_l:')
+  const parseNum = num => (num === '2' ? ':regional_indicator_w:' : num === '1' ? ':regional_indicator_d:' : ':regional_indicator_l:')
 
   const results = [ s.lastMatch0, s.lastMatch1, s.lastMatch2, s.lastMatch3, s.lastMatch4, s.lastMatch5 ]
 
